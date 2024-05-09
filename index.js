b8d50cdd26f85ad8aa777db953a676b216cd8ad6
ImmortalSul <157292013+ImmortalSul@users.noreply.github.com>
2024-05-10 03:03:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+
+/* DEPLOYMENT */
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 10001;
 mongoose
